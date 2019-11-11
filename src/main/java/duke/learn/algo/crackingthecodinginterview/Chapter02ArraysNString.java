e844774e888fd44e77954dc7393f0154e0be8e6c
Kazi Abid Azad <kaziabidazad@gmail.com>
2019-11-11 09:46:31 +0530

    c01 q3 added

--- src/main/java/duke/learn/algo/crackingthecodinginterview/Chapter02ArraysNString.java
+++ src/main/java/duke/learn/algo/crackingthecodinginterview/Chapter02ArraysNString.java
@@ -7,7 +7,78 @@ package duke.learn.algo.crackingthecodinginterview;
  */
 public class Chapter02ArraysNString {
 
-    public static void main(String[] args) {
+    /**
+     * <b>1. </b> Implement an algorithm to determine if a string has all unique
+     * characters. What if you cannot use additional data structure?<br>
+     * Time :<b> O(N) </b>- we can also argue <b>O(1) </b>because the for loop will
+     * never exceed 128 iterations <br>
+     * Space : <b>O(1) </b>
+     * 
+     * @param input
+     * @return
+     */
+    static boolean isUnique(String input) {
+	int[] allChars = new int[128];
+	char[] inputChars = input.toCharArray();
+	for (int i = 0; i < inputChars.length; i++) {
+	    char c = inputChars[i];
+	    if (allChars[c] > 0)
+		return false;
+	    allChars[c] = c;
+	}
+	return true;
+    }
 
+    /**
+     * <b>2. </b>Given 2 Strings, write a method to decide if one is permutation of
+     * another <br>
+     * 
+     * @param str1
+     * @param str2
+     * @return
+     */
+    static boolean checkPermutation(String str1, String str2) {
+	if (str1.length() != str2.length())
+	    return false;
+	int[] chars = new int[128];
+	char[] sChars = str1.toCharArray();
+	for (char c : sChars)
+	    chars[c]++;
+	sChars = str2.toCharArray();
+	for (char c : sChars) {
+	    chars[c]--;
+	    if (chars[c] < 0)
+		return false;
+	}
+	return true;
+    }
+
+    /**
+     * <b>3. </b> Write a method to replace all spaces in a String with '%20'. You
+     * may assume that the string has sufficient spaces at the end to hold the
+     * additional characters, and that you are given the true length of the String.
+     * 
+     * @param url
+     * @return
+     */
+    static String urlify(String url) {
+	int len = url.length();
+	char[] chars = url.trim().toCharArray();
+	char[] out = new char[len];
+	for (int i = 0, j = 0; i < chars.length; i++) {
+	    if (chars[i] == ' ') {
+		out[j++] = '%';
+		out[j++] = '2';
+		out[j++] = '0';
+	    } else
+		out[j++] = chars[i];
+	}
+	return new String(out);
+    }
+
+    public static void main(String[] args) {
+	// System.out.println(isUnique("Kazi biid"));
+	// System.out.println(checkPermutation("kazii", "zikai"));
+	System.out.println(urlify("Mr John Smiths    "));
     }
 }
