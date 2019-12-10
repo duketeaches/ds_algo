7c86cb4ff57f31d215dd4720818088e764d5d422
Kazi Abid Azad <kaziabidazad@gmail.com>
2019-12-10 06:52:35 +0530

    utils

--- src/main/java/duke/learn/algo/crackingthecodinginterview/Chapter03StacksAndQueues.java
+++ src/main/java/duke/learn/algo/crackingthecodinginterview/Chapter03StacksAndQueues.java
@@ -3,6 +3,7 @@
  */
 package duke.learn.algo.crackingthecodinginterview;
 
+import java.util.NoSuchElementException;
 import java.util.Stack;
 
 /**
@@ -76,4 +77,51 @@ public class Chapter03StacksAndQueues {
 	}
 
     }
+
+    private static class Queue<T> {
+
+	private QueueNode<T> first;
+	private QueueNode<T> last;
+
+	public void add(T item) {
+	    QueueNode<T> t = new QueueNode<>(item);
+	    if (last != null)
+		last.next = t;
+	    last = t;
+	    if (first == null)
+		first = last;
+	}
+
+	public T remove() {
+	    if (first == null)
+		throw new NoSuchElementException();
+	    T data = first.data;
+	    first = first.next;
+	    if (first == null)
+		last = null;
+	    return data;
+	}
+
+	public T peek() {
+	    if (first == null)
+		throw new NoSuchElementException();
+	    return first.data;
+
+	}
+
+	public boolean isEmpty() {
+	    return first == null;
+	}
+
+	private static class QueueNode<T> {
+	    private T data;
+	    private QueueNode<T> next;
+
+	    public QueueNode(T data) {
+		super();
+		this.data = data;
+	    }
+	}
+    }
+
 }
